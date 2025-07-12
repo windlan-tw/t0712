015a6e296dc8cb52d21a756d1e3d2f8477e3ab5a
windlan-tw <windlan@hotmail.com>
2025-07-12 16:26:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,21 +29,3 @@ def pricing(keyword: str, sort_by: str = None):
     if sort_by == "pricing":
         data.sort(key=get_pricing, reverse=True)
     return data
-
-def read_main():
-    return {"msg": "Hello World"}
-
-
-def get_pricing(obj):
-    return obj["pricing"]
-
-
-@app.get("/pricing")
-def pricing(keyword: str, sort_by: str = None):
-    elite_data = shopping.query_elite(keyword)
-    pchome_data = shopping.query_pchome(keyword)
-    data = pchome_data + elite_data
-    if sort_by == "-pricing":
-        data.sort(key=get_pricing)
-        # data = sorted(data, key=get_pricing)
-    return data
